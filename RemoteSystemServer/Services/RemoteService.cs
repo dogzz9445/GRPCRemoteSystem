28dfed4d2afec2f78821ffbe8f56c9045e58770a
Dongmin Jang (장동민) <37837785+dogzz9445@users.noreply.github.com>
2021-10-25 21:47:03 +0900

    add remote

--- RemoteSystemServer/Services/RemoteService.cs
+++ RemoteSystemServer/Services/RemoteService.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Google.Protobuf.WellKnownTypes;
-using RemoteSystem.Remote;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
+using RemoteSystem.Remote;
 
 namespace RemoteSystemServer
 {
     public class RemoteService : Remote.RemoteBase
     {
+        private PerformanceCounter m_cpuPerformanceCounter =
+            new PerformanceCounter("Processor", "% Processor Time", "_Total");
+
+        private PerformanceCounter m_memoryPerformanceCounter =
+            new PerformanceCounter("Memory", "% Committed Bytes in Use");
+
         private readonly ILogger<RemoteService> _logger;
         public RemoteService(ILogger<RemoteService> logger)
         {
@@ -25,5 +32,15 @@ namespace RemoteSystemServer
                 Message = "Hello "
             });
         }
+        public override Task<Performance> GetPerformance(Empty request, ServerCallContext context)
+        {
+            return Task.FromResult(new Performance
+            {
+                CpuUsage = 94.0F,
+                MemoryUsage = 50.0F,
+                NetworkUsage = 100.0F
+            });
+        }
+
     }
 }
